da7f2dbd688848ee2479d18bb0dfb2746e46a444
Edwin Aragon <ared2728@gmail.com>
2020-10-25 16:50:31 -0700

    Add Navbar component and its basic structure. Import Navbar component in Portfolio component

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+
+function Navbar() {
+    return(
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">My Work</a>
+                </li>
+                <li>
+                    <a href="#">About Me</a>
+                </li>
+                <li>
+                    <a href="#">Contact Me</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,9 +1,11 @@
 import './Portfolio.css';
 import Home from './Components/home'
+import Navbar from './Components/Navbar/Navbar'
 
 function Portfolio() {
   return (
     <div>
+      <Navbar />
       <Home />
     </div>
   );
