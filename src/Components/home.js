c04b0b5a1c52a296da5725bfba27d7187922b977
Edwin Aragon <ared2728@gmail.com>
2020-10-25 17:17:25 -0700

    Remove both sides margin from Home component and set it at page label (index.css). Set display to flex for easy positioning of elements inside Home component

--- src/Components/home.js
+++ src/Components/home.js
@@ -2,14 +2,17 @@ import './home.css';
 
 function Home() {
     return(
-        <div className="welcome-message">
-            <h1 className="title">Hi there! I'm Edwin Aragón</h1>
-            <p className="self-description">
-                I'm a web developer who likes to code and stay up to date with the technology. 
-                I also enjoy blogging about my experience and my journey to become better every day.
-                I like to help others sharing my knowledge.
-            </p>
-            <p className="self-description">Want to know more about me? Keep scrolling!</p>
+        <div style={{display:"flex"}}>
+            <div>
+                <h1 className="title">Hi there! I'm Edwin Aragón</h1>
+                <p className="self-description">
+                    I'm a web developer who likes to code and stay up to date with the technology. 
+                    I also enjoy blogging about my experience and my journey to become better every day.
+                    I like to help others sharing my knowledge.
+                </p>
+                <p className="self-description">Want to know more about me? Keep scrolling!</p>
+            </div>
+            <img src="images/dev_productivity.svg" alt="" />
         </div>
     );
 }
