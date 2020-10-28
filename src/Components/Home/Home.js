0e4fcce4ba499f0a947f46851958cf3feba0106f
Edwin Aragon <ared2728@gmail.com>
2020-10-27 19:37:02 -0700

    Update copy for Home component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,8 +7,8 @@ function Home() {
                 <h1 className="title">Hi there! I'm Edwin Aragón</h1>
                 <p className="self-description">
                     I'm a web developer who likes to code and stay up to date with the technology. 
-                    I also enjoy blogging about my experience and my journey to become better every day.
-                    I like to help others sharing my knowledge.
+                    I also enjoy blogging about my experience and my journey to become better developer every day.
+                    I like to help others by sharing my knowledge.
                 </p>
                 <p className="self-description">Want to know more about me? Keep scrolling!</p>
             </div>
