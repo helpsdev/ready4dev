7fbbf3d7effb03896d46aad130c7ec3c9e4c106b
Edwin Aragon <ared2728@gmail.com>
2020-10-28 19:57:42 -0700

    Add Education and Experience to the About Me component

--- src/Components/AboutMe/aboutMe.js
+++ src/Components/AboutMe/aboutMe.js
@@ -3,12 +3,49 @@ function AboutMe() {
     return(
         <section>
             <h2>About Me</h2>
+            <h3>Education</h3>
+            <p>
+                I studied at the Autonomus University of Chihuahua, I got the Systems Enginieer degree specialized in Hardware.
+            </p>
+            <p>
+                I've been working in the web development field for 4+ years now. I feel like I learned a lot in these yeras but at the same time
+                that there is a lot more to learn.
+            </p>
+            <p>
+                I have a Microsoft Certified Solutions Associate certification in Web Applications 
+                that you can check out <a href="https://www.youracclaim.com/badges/056eb240-185c-435c-be4f-865c8b34af5f/linked_in_profile">here</a>.
+            </p>
+            <h3>Experience</h3>
+            <p>
+                I started my journey as web developer as an internship in Datazone (an Autozone company). 
+                I worked with both Back-end and front-end technologies to complete a project that will help the call center deparment
+                to visualize with ease a special type of tickets logged in the ticketing system used there.
+            </p>
+            <p>
+                After that I moved to Mexicali, Baja California, Mexico to be part of the development team of Grupo Red Internet Development, a company dedicated to build software on demand.
+                Here I worked also with both Back-end and front-end technologies.
+            </p>
+            <p>
+                Then I moved back to Chihuahua, Mexico where I joined the Gammapartners team, here is where I'm currently working. 
+                I've also worked with both Back-end and front-end tecnhonologies.
+                Last year we joined the PK Global workforce so we are part of their team now.
+            </p>
+            <h3>Skills</h3>
+            <p>In all these years working as web developer I've learned several different languages and frameworks showing below:</p>
+            add a section with the logos of the skills I have experience on
+            <h3>My history</h3>
             <p>
                 I'm a web developer based in Mexico. Since I was little I was amazed by the computers. 
-                I loved spending time on them and investigating how they work. 
-                I had to say that more than once I had to reinstall the OS because of touching things I shoudn't or at least not without knowing what could happen.
-                I'm passionate about learning new things and that's why I think I love being a developer. There is always room for improvement, things
-                are constantly changing so you always have something new to learn.
+                I loved spending time on them and learning how they work (and I'm still learning!). 
+                I had to say that more than once I had to reinstall the OS because of touching things I shoudn't or not without knowing what could happen if I touched them.
+                I'm passionate about learning new things and that's why I think I love being a developer. In this career there is always room for improvement, things
+                are constantly changing so you have to be constantly learning.
+            </p>
+            <p>
+                I know my specialization says "Hardware" but believe me, when I was stuidying for my degree there was a lot of programming courses I had to take.
+                I always have been in touch with programming since I was in highschool but never took it seriously until I had the chance to 
+                do my internship at Datazone. While I was there I had the opportunity to learn a lot of stuff that lead me to the absolute decision
+                that I had to become a web developer.
             </p>
         </section>
     );
