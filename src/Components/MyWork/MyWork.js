fd13f4de0b33bc355bee35c42c4927a9df2e311f
Edwin Aragon <ared2728@gmail.com>
2020-11-01 15:00:49 -0700

    Display more information about projects using sections.

--- src/Components/MyWork/MyWork.js
+++ src/Components/MyWork/MyWork.js
@@ -1,31 +1,64 @@
+import "./myWork.css"
 
 function MyWork() {
     return(
         <section>
             <h2>My Work</h2>
-            <ul>
+            <section className="projects">
                 {
                     getProjectList().map(p => {
-                        return (
-                            <li>Name: {p.name}</li>
+                        return (                        
+                            <section>
+                                {p.preview && <img src={p.preview}></img>}
+                                <p>{p.name}</p>
+                                <p>{p.description}</p>
+                                <p>{p.technologies.join(" | ")}</p>
+                                <a href={p.liveApp}>Live Site</a>
+                                <a href={p.code}>Github</a>
+                            </section>
                         );
                     })
                 }
-            </ul>
+            </section>
         </section>
     );
 }
 
+
 function getProjectList(){
     return [
         {
-            name: "Chatbot"
+            name: "Chatbot",
+            code:"",
+            liveApp:"",
+            preview:"",
+            description:"",
+            technologies:[
+                "HTML",
+                "CSS",
+                "JS"
+            ]
         },
         {
-            name: "Stock Market Simulator"
+            name: "Stock Market Simulator",
+            code:"",
+            liveApp:"",
+            preview:"",
+            description:"",
+            technologies:[
+                "HTML",
+                "CSS",
+                "JS",
+                "React"
+            ]
         },
         {
-            name: "Home Donuts"
+            name: "Home Donuts",
+            code:"",
+            liveApp:"",
+            preview:"",
+            description:"",
+            technologies:[]
         }
     ]
 }
